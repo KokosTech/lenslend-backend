812d94080eafd6a8a88896e66c50d8c003a44fbb
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-12-25 18:40:51 +0200

    ci(#2): Add swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('LensLend')
+    .setDescription('The LensLend API description')
+    .setVersion(process.env.VERSION || '0.0.0')
+    .addBearerAuth()
+    .addSecurity('refresh_token', {
+      type: 'http',
+      scheme: 'bearer',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
