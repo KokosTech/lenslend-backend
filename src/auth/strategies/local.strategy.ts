7dbb46fb7d9db6023226ba82bbf36a8c0f999e25
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-12-25 18:05:17 +0200

    feat(#5): Add local-auth guard and strategy

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(username: string, password: string) {
+    const user = await this.authService.validateUser(username, password);
+
+    console.log('AAAAAAABBBBBBв');
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
