a41199dc89a44f44fb037a58c8739b5c5f601525
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-01-18 00:00:52 +0200

    feat(#15): Personal user profile page

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import type { User } from '@prisma/client';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RequestWithUser } from '../common/interfaces/RequestWithUser';
 
 @Controller('user')
 // @UseGuards(JwtAuthGuard)
@@ -10,6 +11,14 @@ import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 @ApiBearerAuth()
 export class UserController {
   constructor(private prisma: PrismaService) {}
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getMe(@Req() req: RequestWithUser): Promise<User> {
+    console.warn('AAAAAAAAAAAA');
+    console.log(req.user);
+    return req.user;
+  }
 
   @Get()
   @UseGuards(JwtAuthGuard)
