c8c916c9a7bbe9fea9ef81dc3399025c7b0a4522
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-02-03 18:26:10 +0200

    feat(#18): add file controller

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { FileService } from './file.service';
 import { FileDto } from './dtos/file.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RequestWithUser } from '../common/interfaces/RequestWithUser';
+import { FileInterface, FileResponseDto } from './interface/file.interface';
 
 @Controller('file')
 @ApiTags('file')
@@ -13,7 +14,15 @@ export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Get('upload')
-  async uploadFile(@Req() req: RequestWithUser, @Query() query: FileDto) {
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    type: FileResponseDto,
+  })
+  async uploadFile(
+    @Req() req: RequestWithUser,
+    @Query() query: FileDto,
+  ): Promise<FileInterface> {
     return this.fileService.uploadFile(query, req.user.uuid);
   }
 }
--- src/file/interface/file.interface.ts
+++ src/file/interface/file.interface.ts
@@ -1,5 +1,25 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export interface FileInterface {
   url: string;
   key: string;
   public_url: string;
+}
+
+export class FileResponseDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  readonly key: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  readonly url: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  readonly public_url: string;
 }
